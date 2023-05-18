241f2c5e382b37844afe17cd145d1b6d5df820ed
xintakazi <2078106357@qq.com>
2023-05-18 20:41:17 +0800

    nvim与ranger初始配置

---
+++ nvim/README.md
@@ -0,0 +1,1 @@
+# Xintakazi Nvim配置
---
+++ ranger/BaseProgInstall.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+sudo apt-get install ranger caca-utils highlight atool w3m  mediainfo -y # ranger 的主程序 img2txt 图片 代码高亮 存档预览 html页面预览 多媒体文件预览
+
+sudo apt-get install libx11-dev libxext-dev
+pip install ueberzug
+
