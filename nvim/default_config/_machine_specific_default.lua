c114a1b80e3ba41766b816a1e5c2cda23a9d97c0
Davis Ye <davis_ye@foxmail.com>
2023-07-08 14:58:03 +0800

    upgrade nvim config && joshuto config

---
+++ nvim/default_config/_machine_specific_default.lua
@@ -0,0 +1,14 @@
+-- please read through this file and change what you need to change
+
+--vim.g.old.snips_author = 'David Chen'
+vim.g.snips_author = 'Davis Ye'
+
+vim.g.python3_host_prog = '/home/davis/.venv/bin/python3'
+
+--[[
+vim.g.mkdp_browser = 'chromium'
+vim.g.mkdp_browserfunc = 'open '
+--]]
+
+vim.g.flutter_default_device = 'arch'
+vim.g.flutter_run_args = ''
